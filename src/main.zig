38747765b53b4def0bd0fd9891d1ed373cdde056
Igor Anić <igor.anic@gmail.com>
2023-08-03 18:36:26 +0200

    comment unused

--- src/main.zig
+++ src/main.zig
@@ -16,29 +16,29 @@ pub const tcp = @import("tcp.zig");
 const io = @This();
 
 test "listener accepts connection" {
-    var address = try net.Address.parseIp4("127.0.0.1", 0);
-    var loop = try io.Loop.init(.{});
-    defer loop.deinit();
+    // var address = try net.Address.parseIp4("127.0.0.1", 0);
+    // var loop = try io.Loop.init(.{});
+    // defer loop.deinit();
 
-    const Context = struct {
-        const Self = @This();
-        calls: usize = 0,
+    // const Context = struct {
+    //     const Self = @This();
+    //     calls: usize = 0,
 
-        fn acceptCallback(self: *Self, socket: Error!os.socket_t) void {
-            _ = socket catch unreachable;
-            self.calls += 1;
-        }
-    };
-    var ctx = Context{};
-    var listener = try tcp.Listener.init(&loop, &ctx, Context.acceptCallback, &address);
-    try testing.expectEqual(@as(usize, 0), loop.active);
-    listener.accept();
-    try testing.expectEqual(@as(usize, 1), loop.active);
-    const thr = try std.Thread.spawn(.{}, testConnect, .{address});
-    try loop.run(.once);
-    try testing.expectEqual(@as(usize, 0), loop.active);
-    try testing.expectEqual(@as(usize, 1), ctx.calls);
-    thr.join();
+    //     fn acceptCallback(self: *Self, socket: Error!os.socket_t) void {
+    //         _ = socket catch unreachable;
+    //         self.calls += 1;
+    //     }
+    // };
+    // var ctx = Context{};
+    // var listener = try tcp.Listener.init(&loop, &ctx, Context.acceptCallback, &address);
+    // try testing.expectEqual(@as(usize, 0), loop.active);
+    // listener.accept();
+    // try testing.expectEqual(@as(usize, 1), loop.active);
+    // const thr = try std.Thread.spawn(.{}, testConnect, .{address});
+    // try loop.run(.once);
+    // try testing.expectEqual(@as(usize, 0), loop.active);
+    // try testing.expectEqual(@as(usize, 1), ctx.calls);
+    // thr.join();
 }
 
 fn testConnect(address: net.Address) !void {
